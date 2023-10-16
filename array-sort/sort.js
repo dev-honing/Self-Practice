b73283ed65cd2234b6b19978e53a4a56c8d0fea2
Tech1997 <dev.honing@gmail.com>
2023-10-16 19:27:00 +0900

    문자열은 a-z까지 오름차순으로 정렬이 잘된다.

--- array-sort/sort.js
+++ array-sort/sort.js
@@ -1,1 +1,5 @@
-console.log("hello");
+// 231016 알게된 사실
+// array.sort() 메서드는 문자열을 정렬하는 메서드이다.
+const arr = ['apple', 'telephone', 'question', 'banana', 'pear', 'answer'];
+console.log(arr.sort()); // a-z까지 오름차순 정렬하는 메서드
+
