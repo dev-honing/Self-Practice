6b865b6019ba823486dc3f1dbefdd4889fcb9db9
Tech1997 <dev.honing@gmail.com>
2023-10-16 21:04:43 +0900

    함수 random()의 로직 작성

--- Math-method/math.js
+++ Math-method/math.js
@@ -2,8 +2,6 @@
 // Q. 1~15 중 랜덤의 번호를 출력하려면 어떻게 해야할까?
 // A. Math.random() 메서드를 활용한다
 
-// 배열을 변수 선언
-const arr = [1, 2, 3, 4, 5];
 // Math.random() 메서드는 0~1 중 랜덤의 값을 뽑아주는 메서드이다.(1은 미포함)
 
 // console.log(Math.random()); // 소숫점이 있는 랜덤 값이 출력됨을 확인
@@ -12,7 +10,20 @@ const arr = [1, 2, 3, 4, 5];
 // console.log(Math.floor(Math.random())); // 0이 출력됨
 
 // 10을 곱하고 함수화 한 뒤, 계속 찍어보면?
-function randomNum(){
-  console.log(Math.floor(Math.random()*10));
+// function randomNum(){
+//   console.log(Math.floor(Math.random()*10));
+// }
+// setInterval(randomNum, 100) // 0~9까지 출력됨을 확인
+
+// 범위를 조정하려면?
+// 새로운 배열을 선언하고 배열에 요소가 있는지 확인해보자.
+function random(){
+  const arr = [];
+  const randomNum = Math.floor(Math.random()*10);
+  // 조건식 작성: 배열 arr의 요소 중에 randomNum의 값이 없으면 arr.push() 메서드로 요소를 생성
+  if(arr.indexOf(randomNum) === -1 ){
+    arr.push(randomNum);
+  }
+  console.log(arr);
 }
-setInterval(randomNum, 100) // 0~9까지 출력됨을 확인
+setInterval(random, 100);
